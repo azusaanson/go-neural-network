df124f101e7ab4ef17685c3a02889eadd9b04ac9
azusaanson <yth041512@gmail.com>
2023-06-01 16:10:38 +0900

    unit, stepFunc, sigmoidFunc

---
+++ basic.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+const e float64 = math.E
+
+func getUnit(wyMap map[float64]float64, b float64, activationFunc func(float64) float64) float64 {
+	var s float64 = 0
+	for weight, y := range wyMap {
+		s = s + weight*y
+	}
+	return activationFunc(s)
+}
+
+func stepFunc(x float64) (y float64) {
+	y = 0
+	if x >= 0 {
+		y = 1
+	}
+	return
+}
+
+func sigmoidFunc(x float64) (y float64) {
+	y = 1 / (1 + math.Pow(e, -x))
+	return
+}
