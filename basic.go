790db1434673d6459f27c7d4c5cfb49bb38d081e
azusaanson <yth041512@gmail.com>
2023-06-02 01:30:34 +0900

    fix

--- basic.go
+++ basic.go
@@ -1,11 +1,9 @@
 package main
 
-import "math"
-
-type modelWithData struct {
-	*data
-	*model
-}
+import (
+	"errors"
+	"math"
+)
 
 type data struct {
 	xs [][]float64
@@ -13,65 +11,100 @@ type data struct {
 }
 
 type model struct {
-	layerW         [][]float64
-	layerB         []float64
-	defaultW       float64
-	defaultB       float64
+	layerUnitWs    [][][]float64
+	layerUnitB     [][]float64
 	activationFunc func(float64) float64
+	layerUnitNums  []int
 }
 
-func newModel(layerW [][]float64, layerB []float64, defaultW float64, defaultB float64, activationFunc func(float64) float64) *model {
+func newData(xs [][]float64, zs []float64) *data {
+	return &data{
+		xs: xs,
+		zs: zs,
+	}
+}
+
+func newModel(layerUnitWs [][][]float64, layerUnitB [][]float64, activationFunc func(float64) float64, layerUnitNums []int) *model {
 	return &model{
-		layerW:         layerW,
-		layerB:         layerB,
-		defaultW:       defaultW,
-		defaultB:       defaultB,
+		layerUnitWs:    layerUnitWs,
+		layerUnitB:     layerUnitB,
 		activationFunc: activationFunc,
+		layerUnitNums:  layerUnitNums,
 	}
 }
 
-func (m *model) fit(xs [][]float64, zs []float64) *modelWithData {
-	return &modelWithData{
-		data: &data{
-			xs: xs,
-			zs: zs,
-		},
-		model: m,
+func createModel(k int, layerUnitNums []int, activationFunc func(float64) float64, defaultW float64, defaultB float64) *model {
+	layerNums := len(layerUnitNums)
+
+	newLayerUnitWs := make([][][]float64, layerNums)
+	for i := 0; i < len(newLayerUnitWs); i++ {
+		newLayerUnitWs[i] = make([][]float64, layerUnitNums[i])
+		for j := 0; j < len(newLayerUnitWs[i]); j++ {
+			inputNums := k
+			if i != 0 {
+				inputNums = layerUnitNums[i-1]
+			}
+			newLayerUnitWs[i][j] = make([]float64, inputNums)
+			for h := 0; h < len(newLayerUnitWs[i][j]); h++ {
+				newLayerUnitWs[i][j][h] = defaultW
+			}
+		}
 	}
+
+	newlayerUnitB := make([][]float64, layerNums)
+	for i := 0; i < len(newLayerUnitWs); i++ {
+		newlayerUnitB[i] = make([]float64, layerUnitNums[i])
+		for j := 0; j < len(newLayerUnitWs[i]); j++ {
+			newlayerUnitB[i][j] = defaultB
+		}
+
+	}
+
+	return newModel(newLayerUnitWs, newlayerUnitB, activationFunc, layerUnitNums)
 }
 
 const e float64 = math.E
 
-func (md *modelWithData) predUnit(ys []float64, ws []float64, b float64) float64 {
+func (m *model) predUnit(ys []float64, ws []float64, b float64) float64 {
 	var s float64 = 0
 	for i := 0; i < len(ys); i++ {
 		s = s + ws[i]*ys[i]
 	}
 	s = s - b
-	return md.activationFunc(s)
+	return m.activationFunc(s)
 }
 
-func (md *modelWithData) predLayer(isNew bool, layer int, inputs []float64, outputNums int) []float64 {
+func (m *model) predLayer(layer int, inputs []float64, outputNums int) []float64 {
 	ys := make([]float64, outputNums)
 
-	if isNew {
-		md.layerW[layer] = make([]float64, len(inputs))
-		for i := 0; i < len(md.layerW[layer]); i++ {
-			md.layerW[layer][i] = md.defaultW
-		}
-		md.layerB[layer] = md.defaultB
-	}
-
-	ws := md.layerW[layer]
-	b := md.layerB[layer]
+	ws := m.layerUnitWs[layer]
+	b := m.layerUnitB[layer]
 
 	var y float64
 	for i := 0; i < outputNums; i++ {
-		y = md.predUnit(inputs, ws, b)
+		y = m.predUnit(inputs, ws[i], b[i])
 		ys = append(ys, y)
 	}
 
 	return ys
+}
+
+func (m *model) pred(x []float64) (y float64, err error) {
+	if len(x) != len(m.layerUnitWs[0][0]) {
+		return 0, errors.New("invalid x length")
+	}
+
+	// hidden layers
+	var layerY []float64 = x
+	for i := 0; i < len(m.layerUnitWs)-1; i++ {
+		layerY = m.predLayer(i, layerY, len(m.layerUnitWs[i+1]))
+	}
+
+	// output layer
+	layerY = m.predLayer(len(m.layerUnitWs)-1, layerY, 1)
+	y = layerY[0]
+
+	return y, nil
 }
 
 func stepFunc(x float64) (y float64) {
@@ -79,10 +112,10 @@ func stepFunc(x float64) (y float64) {
 	if x >= 0 {
 		y = 1
 	}
-	return
+	return y
 }
 
 func sigmoidFunc(x float64) (y float64) {
 	y = 1 / (1 + math.Pow(e, -x))
-	return
+	return y
 }
