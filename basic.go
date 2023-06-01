aaf098e0438fee5359555dba5801fbf48e7459a2
azusaanson <yth041512@gmail.com>
2023-06-02 00:22:22 +0900

    fix

--- basic.go
+++ basic.go
@@ -2,8 +2,6 @@ package main
 
 import "math"
 
-const e float64 = math.E
-
 type modelWithData struct {
 	*data
 	*model
@@ -22,7 +20,7 @@ type model struct {
 	activationFunc func(float64) float64
 }
 
-func NewModel(layerW [][]float64, layerB []float64, defaultW float64, defaultB float64, activationFunc func(float64) float64) *model {
+func newModel(layerW [][]float64, layerB []float64, defaultW float64, defaultB float64, activationFunc func(float64) float64) *model {
 	return &model{
 		layerW:         layerW,
 		layerB:         layerB,
@@ -42,7 +40,9 @@ func (m *model) fit(xs [][]float64, zs []float64) *modelWithData {
 	}
 }
 
-func (md *modelWithData) unit(ys []float64, ws []float64, b float64) float64 {
+const e float64 = math.E
+
+func (md *modelWithData) predUnit(ys []float64, ws []float64, b float64) float64 {
 	var s float64 = 0
 	for i := 0; i < len(ys); i++ {
 		s = s + ws[i]*ys[i]
@@ -51,15 +51,23 @@ func (md *modelWithData) unit(ys []float64, ws []float64, b float64) float64 {
 	return md.activationFunc(s)
 }
 
-func (md *modelWithData) layer(layer int, inputs []float64, outputNums int) []float64 {
+func (md *modelWithData) predLayer(isNew bool, layer int, inputs []float64, outputNums int) []float64 {
 	ys := make([]float64, outputNums)
+
+	if isNew {
+		md.layerW[layer] = make([]float64, len(inputs))
+		for i := 0; i < len(md.layerW[layer]); i++ {
+			md.layerW[layer][i] = md.defaultW
+		}
+		md.layerB[layer] = md.defaultB
+	}
 
 	ws := md.layerW[layer]
 	b := md.layerB[layer]
 
 	var y float64
 	for i := 0; i < outputNums; i++ {
-		y = md.unit(inputs, ws, b)
+		y = md.predUnit(inputs, ws, b)
 		ys = append(ys, y)
 	}
 
